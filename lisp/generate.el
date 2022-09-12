147a36ac4fbc658d2886cb27931318aab9d7765a
Jen-Chieh Shen <jcs090218@gmail.com>
2022-09-12 21:49:47 +0800

    feat(genrate.el): attempt to build

--- lisp/generate.el
+++ lisp/generate.el
@@ -9,6 +9,18 @@
 
 ;;; Code:
 
+;; Don't call, but loaded for cache (speed)
+(eask-load "core/package")
 
+;; If the packaged file doesn't exists, try to build one
+(unless (eask-packaged-file) (eask-call "core/package"))
+
+(unless (eask-packaged-file)
+  (eask-error "Failed to build artifact, make sure your are able to build your package"))
+
+(defconst nix-hash-format "nix-hash --flat --base32 --type sha256 %s"
+  "Format string to hash the tar/el file.")
+
+(shell-command-to-string (format nix-hash-format (eask-packaged-file)))
 
 ;;; generate.el ends here
